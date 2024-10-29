6fecdd96a2040f089caab464cda372fa3b0d8289
Justin-N-Ho <hojus000@auburnsd.org>
2024-10-29 14:06:56 -0700

    constants

---
+++ src/main/java/frc/robot/constants/Control.java
@@ -0,0 +1,17 @@
+package frc.robot.constants;
+
+public class Control {
+
+    public class drivetrain {
+        public static final double GEAR_RATIO = 0; //TODO find
+        public static final double WHEEL_DIAMETER = 6; //inches
+
+        public static final double kVelocityConversionFactor = 0; //TODO find
+        public static final double kMaxVel = 0; //TODO find
+        
+        public static final double kP = 0.001;
+        public static final double kI = 0.001;
+        public static final double kD = 0.001;
+        public static final double kFF = 0.001;
+    }
+}
---
+++ src/main/java/frc/robot/constants/Ports.java
@@ -0,0 +1,10 @@
+package frc.robot.constants;
+
+public class Ports {
+    public class drivetrain {
+        public static final int FRONT_LEFT = 1;
+        public static final int REAR_LEFT = 2;
+        public static final int FRONT_RIGHT = 3;
+        public static final int REAR_RIGHT = 4;
+    }
+}
