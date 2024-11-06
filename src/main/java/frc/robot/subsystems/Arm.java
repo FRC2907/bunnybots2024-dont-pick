2f4be8c54ecc9b4ef51c13a2532923f88f6932ee
OverInfrared <54323280+OverInfrared@users.noreply.github.com>
2024-11-05 16:25:03 -0800

    Added arm setSetPoint functions

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,9 +1,11 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.ControlType;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.math.geometry.Translation2d;
+import frc.robot.Util;
 import frc.robot.constants.Control;
 import frc.robot.constants.Ports;
 
@@ -17,9 +19,30 @@ public class Arm implements ISubsystem{
         armExtend = new CANSparkMax(Ports.arm.ARM_EXTENSION, MotorType.kBrushless);
     }
 
-    public void getSetPointAngle(Translation2d setpoint){
-        double xDifference = setpoint.getX() - Control.arm.ARM_POSITION.getX();
-        double yDifference = setpoint.getY() - Control.arm.ARM_POSITION.getY();
+    public double getSetPointAngle(Translation2d _setPoint){
+        double xDifference = _setPoint.getX() - Control.arm.ARM_POSITION.getX();
+        double yDifference = _setPoint.getY() - Control.arm.ARM_POSITION.getY();
+        double hypotenuse = Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2));
+
+        return Math.asin(yDifference/hypotenuse);
+    }
+
+    public void setSetPoint(Translation2d _setPoint){
+        double x = Util.clamp(0, _setPoint.getX(), Control.arm.MAX_ARM_EXTENSION_SIDE);
+        double y = Util.clamp(0, _setPoint.getY(), Control.arm.MAX_ARM_EXTENSION_TOP);
+        double hypotenuse = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        double angle = getSetPointAngle(new Translation2d(x, y));
+
+        arm.getPIDController().setReference(angle, ControlType.kPosition);
+        armExtend.getPIDController().setReference(hypotenuse, ControlType.kPosition);
+    }
+
+    public void setSetPoint(double angle){
+        double x = Control.arm.MAX_ARM_EXTENSION_SIDE;
+        double hypotenuse = x/Math.cos(angle);
+        
+        arm.getPIDController().setReference(angle, ControlType.kPosition);
+        armExtend.getPIDController().setReference(hypotenuse, ControlType.kPosition);
     }
 
     @Override
